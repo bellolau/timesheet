9a16db77b8c910449bc4b9107369a65f44794db8
Laurent Bello <bellolau@gmail.com>
2012-10-08 21:47:41 +0200

    feat: (Bootstrap) clean feat: (ProjectController) return json response for ajax calls

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,9 +3,6 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
     protected function _initAutoload() {
-        //$this->bootstrap("frontController");
-        //$front = $this->frontController;
-
         $autoloader = Zend_Loader_Autoloader::getInstance();
         $autoloader->registerNamespace('My_');
 
